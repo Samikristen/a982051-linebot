5b0416b2d5298da7c9812f5958309833022e4148
sami <apherk2945@gmail.com>
2023-05-09 15:33:19 +0800

    init

---
+++ a982051-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace a982051_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
