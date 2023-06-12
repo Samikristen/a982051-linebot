4cb3419848bd34572b221bf5a2c793f98faaf0f3
sami <apherk2945@gmail.com>
2023-06-12 14:23:08 +0800

    改回應

--- a982051-linebot/LineBotApp.cs
+++ a982051-linebot/LineBotApp.cs
@@ -6,15 +6,18 @@ namespace A982051_linebot;
 public class LineBotApp : WebhookApplication
 {
     private readonly LineMessagingClient _messagingClient;
+ 
+    private static Dictionary<string, string> _pool = new Dictionary<string, string>();
+ 
     public LineBotApp(LineMessagingClient lineMessagingClient)
     {
         _messagingClient = lineMessagingClient;
     }
-
+ 
     protected override async Task OnMessageAsync(MessageEvent ev)
     {
         var result = null as List<ISendMessage>;
-
+ 
         switch (ev.Message)
         {
             //文字訊息
@@ -24,32 +27,107 @@ public class LineBotApp : WebhookApplication
                 var channelId = ev.Source.Id;
                 //使用者Id
                 var userId = ev.Source.UserId;
-                    
+                //使用者輸入的文字
+                var text = ((TextEventMessage)ev.Message).Text;
+ 
+                if (PoolHasMsg(text))
+                {
+                    // 從記憶體池查詢資料
+                    string response = GetResponse(text);
+                    result = new List<ISendMessage>
+                    {
+                        new TextMessage(response)
+                    };
+                }
+                else
+                {
+                    if (CheckFormat(text))
+                    {
+                        //將資料寫入記憶體池
+                        TeachDog(text);
+                        result = new List<ISendMessage>
+                        {
+                            new TextMessage("自己學，不要依靠我小廢物")
+                        };
+                    }
+                }
+ 
+                /*
                 //回傳 hellow
                 result = new List<ISendMessage>
                 {
-                    new TextMessage("你怎麼這麼可憐，只能跑來和機器人說話")
+                    new TextMessage("你寂寞嗎?")
                 };
-                //回傳 B站Vup推薦
-                result = new List<ISendMessage>
-                {
-                    new TextMessage("長庚和尤塞斯，一個可愛夢魔一個日系男二")
-                };
-                //回傳 Vtuber推薦
-                result = new List<ISendMessage>
-                {
-                    new TextMessage("holostars!")
-                };
-                //回傳 NT
-                result = new List<ISendMessage>
-                {
-                    new TextMessage("壓榨盤古和老張的糟糕狒狒")
-                };
+                */
             }
                 break;
         }
-
+ 
         if (result != null)
             await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
+    }
+ 
+    /// <summary>
+    /// 確認是否已經學習過這個對話
+    /// </summary>
+    /// <param name="inputMsg"></param>
+    /// <returns></returns>
+    private bool PoolHasMsg(string inputMsg)
+    {
+        return _pool.ContainsKey(inputMsg);
+    }
+ 
+    /// <summary>
+    /// 用於 已經學習過的對話
+    /// </summary>
+    /// <param name="inputMsg"></param>
+    /// <returns></returns>
+    private string GetResponse(string inputMsg)
+    {
+        return _pool[inputMsg];
+    }
+ 
+    private bool CheckFormat(string inputMsg)
+    {
+        bool result = false;
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            //檢查
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "負能量鴿子")
+                {
+                    result = true;
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        return result;
+    }
+ 
+    private void TeachDog(string inputMsg)
+    {
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            //檢查
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "負能量鴿子")
+                {
+                    _pool.Add(subs[1],subs[2]);
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
     }
 }
